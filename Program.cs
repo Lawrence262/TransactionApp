b63714601b7fd391a13106eacbea9a6930d23fa4
Lawrence262 <lawrencemanoj99@gmail.com>
2021-06-03 15:15:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductsDAL dal = new ProductsDAL();
+            Products p = new Products();
+            p.Prodid = 78;
+            p.ProductName = "Indian " +
+                "Lays";
+            dal.UpdateProductUsingTranScope(p);
+            Console.WriteLine("Updated Successfully");
+            Console.ReadLine();
+        }
+    }
+}
